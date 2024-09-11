92499ef3aedb4036da8bb7fe4de945eddf967e31
Nennneko5787 <nennneko5787@gmail.com>
2024-09-11 20:03:41 +0900

    gomi

---
+++ cogs/nickname.py
@@ -0,0 +1,62 @@
+import random
+import re
+
+import discord
+from discord.ext import commands
+
+
+class NickNameCog(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.hybrid_command(
+        name="nick", description="サーバーでのニックネームを変更します。"
+    )
+    async def nick(
+        self,
+        ctx: commands.Context,
+        new: str = None,
+        randomnick: bool = False,
+    ):
+        gobi = "。"
+        if (
+            randomnick
+            or "鯖主" in new
+            or "まんこ" in new
+            or re.match(r"(s|S|ｓ|Ｓ)(e|E|ｅ|Ｅ)(x|X|ｘ|Ｘ)", new)
+            or re.match(r"ちん(ぽ|こ|ちん)", new)
+            or re.match(r"(お|オ|ｵ)(な|ナ|ﾅ)(に|ニ|ﾆ)(ー|-|～)", new)
+        ):
+            gobi = "（笑）"
+            new = random.choice(
+                [
+                    "Deleted User",
+                    "ニーハイが食い込んでるの好き♥",
+                    f'{random.choice(["初音ミク", "鏡音リン", "鏡音レン", "巡音ルカ", "MEIKO", "KAITO", "GUMI", "IA"])}大好き♥',
+                    "ブルアカは抜ける",
+                    "ボカロは神。異論は認めない",
+                    "害悪生徒サクッと削除プログラムを起動",
+                    self.bot.get_guild(1282708798791745626)
+                    .get_member(1261969436232126475)
+                    .activity.name,
+                    "猫の喫茶店最高！",
+                    "おい、抜くなよ。",
+                    "初音ミクの胸を盛るな",
+                    "everyone",
+                    "here",
+                    "ゆっくりしていってね",
+                    "下ネタは禁止です",
+                    "エラー！",
+                    "エッチなのはダメ！死刑！！",
+                    "(　´∀｀)ｵﾏｴﾓﾅｰ",
+                    "ﾓﾅ━━━━━━(　´∀｀)━━━━━━!!",
+                    "VSCodeしか勝たん。異論は認めない。",
+                ]
+            )
+        await ctx.author.edit(nick=new)
+        embed = discord.Embed(description=f"ニックネームを`{new}`に変更しました{gobi}")
+        await ctx.reply(embed=embed)
+
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(NickNameCog(bot))
---
+++ main.py
@@ -0,0 +1,35 @@
+import asyncio
+
+import os
+import dotenv
+import sys
+
+import discord
+from discord.ext import commands
+
+dotenv.load_dotenv()
+
+if sys.platform == "win32":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+bot = commands.Bot("jesus!", intents=discord.Intents.all())
+
+
+@bot.command("sync")
+async def sync(ctx: commands.Context):
+    await bot.tree.sync()
+    await ctx.message.delete()
+
+
+@bot.event
+async def setup_hook():
+    await asyncio.gather(
+        *[
+            bot.load_extension(f"cogs.{cog[:-3]}")
+            for cog in os.listdir("cogs")
+            if cog.endswith(".py")
+        ]
+    )
+
+
+bot.run(os.getenv("discord"))
